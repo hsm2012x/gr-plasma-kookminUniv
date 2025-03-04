dcf28c7375fad996e2727b34dc2aa352df9d2f11
Han sumin <hsm2012x@gmail.com>
2025-03-04 12:07:16 +0900

    Backup to build(0304)

--- examples/cmakeTest/plasma_qa_phase_code.cc_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-export VOLK_GENERIC=1
-export GR_DONT_LOAD_PREFS=1
-export srcdir=/home/sumin/git_workspace/gr-plasma-kookminUniv/examples/cmakeTest
-export GR_CONF_CONTROLPORT_ON=False
-export PATH="/home/sumin/git_workspace/gr-plasma-kookminUniv/examples/cmakeTest":"$PATH"
-export LD_LIBRARY_PATH="":$LD_LIBRARY_PATH
-export PYTHONPATH=/home/sumin/git_workspace/gr-plasma-kookminUniv/examples/cmakeTest/test_modules:$PYTHONPATH
-plasma_qa_phase_code.cc 
